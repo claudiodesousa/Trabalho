cc069f81208649462f09e398643ea9ace88ae42e
claudio <claudiodesousa@gmail.com>
2010-12-19 16:18:21 -0200

    more tests

--- src/test/java/app/controllers/SnippetControllerTest.java
+++ src/test/java/app/controllers/SnippetControllerTest.java
@@ -1,36 +1,138 @@
 package app.controllers;
 
 import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import app.models.Snippet;
-import app.repositories.Repository;
 import app.repositories.SnippetRepository;
-import br.com.caelum.vraptor.Delete;
-import br.com.caelum.vraptor.Get;
-import br.com.caelum.vraptor.Path;
-import br.com.caelum.vraptor.Post;
-import br.com.caelum.vraptor.Put;
-import br.com.caelum.vraptor.Resource;
+import app.repositories.SnippetRepositoryImpl;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
-import br.com.caelum.vraptor.view.Results;
+import br.com.caelum.vraptor.util.test.MockResult;
+import br.com.caelum.vraptor.validator.Message;
+import br.com.caelum.vraptor.validator.ValidationException;
+
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SnippetControllerTest {
 
-	@Test public void controllerNotNull(SnippetController sc){
+	@BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    /**
+     * Test of index method, of class SnippetController.
+     */
+    @Test
+    public void testIndex() {
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+        System.out.println("index");
+        Snippet s = new Snippet();
+        SnippetController instance = new SnippetController(r,rs,v);
+        List<Snippet> expResult = new ArrayList<Snippet>();
+        List result = instance.index(s, expResult);
+        assertEquals(expResult, result);
+    }
+    /*
+    @Test
+    public void testSearchByTag() {
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+        System.out.println("index");
+        Snippet s = new Snippet();
+        System.out.println("searchByTag");
+        String tag = "";
+        SnippetController instance = new SnippetController(r,rs,v);
+        instance.searchByTag(tag);
+    }
+    
+    @Test
+    public void testCreate() {
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+		SnippetController sc = new SnippetController(r,rs,v);
+		Snippet s = new Snippet();
+        System.out.println("create");
+        sc.create(s);
+    }
+
+    @Test
+    public void testUpdate() {
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+		SnippetController sc = new SnippetController(r,rs,v);
+		Snippet s = new Snippet();
+        System.out.println("update");
+        Snippet snippet = new Snippet();
+        SnippetController instance = new SnippetController(r,rs,v);
+        instance.update(s);
+    }
+
+    @Test
+    public void testEdit() {
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+		SnippetController sc = new SnippetController(r,rs,v);
+		Snippet s = new Snippet();
+        System.out.println("edit");
+        Snippet snippet = new Snippet();
+        SnippetController instance = new SnippetController(r,rs,v);
+        instance.edit(s);
+    }
+
+    @Test
+    public void testDestroy() {
+        System.out.println("destroy");
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+        Snippet snippet = new Snippet();
+        SnippetController instance = new SnippetController(r,rs,v);;
+        instance.destroy(snippet);
+    }
+	*/
+    @Test public void controllerNotNull(){
+		Result r = null;
+		Validator v = null;
+		SnippetRepository rs = null;
+		SnippetController sc = new SnippetController(r,rs,v);
 		assertNotNull(sc);
 	}
 	
-	@Test public void testCreateFromController(Result r, Validator v, SnippetRepository rs){
-		SnippetController sc = new SnippetController(r,rs,v);
-		Snippet snippet = new Snippet();
-		assertNotNull(snippet);	
-		assertNotNull(rs);
-		assertNotNull(r);
-		assertNotNull(v);
-		sc.create(snippet);
+		
+	@Test public void testNewSnippet(){
+		Result r = null;
+		Validator v = null;
+		EntityManager e = null;
+		SnippetRepository sr = new SnippetRepositoryImpl(e);
+		SnippetController sc = new SnippetController(r,sr,v);
+		
+		Snippet s = new Snippet();
+		assertEquals(s,sc.newSnippet());
 	}
 	
-	
+
 }
+
--- src/test/java/app/models/SnippetTest.java
+++ src/test/java/app/models/SnippetTest.java
@@ -2,10 +2,17 @@ package app.models;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotNull;
+import junit.framework.Assert;
 
+import org.aspectj.lang.annotation.Before;
+import org.hibernate.AssertionFailure;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SnippetTest {
+	
 
     @Test public void testSnippetNotNull() {
     	Snippet s = new Snippet();
@@ -25,6 +32,8 @@ public class SnippetTest {
     	assertEquals("c, for", s.getTags());
     	assertEquals("c", s.getLinguagem());
     }
+    
+
     
     @Test public void testNewSnippetWithoutFieldsShouldFail(){
     	String nome = "aa",linguagem = "c",codigo = "for",tags = "c, for";
@@ -49,6 +58,37 @@ public class SnippetTest {
     	assertNull(s.getLinguagem());
     	assertNull(s.getTags());
     }
+    
+    @Test public void testSnippetWithoutNome(){
+    	String nome = "aa",linguagem = "c",codigo = "for",tags = "c, for";
+    	Snippet s = new Snippet();
+    	s.setLinguagem(linguagem);
+    	s.setCodigo(codigo);
+    	s.setTags(tags);
+    	assertNull(s.getNome());
+    	assertNotNull(s.getCodigo());
+    	assertNotNull(s.getTags());
+    	assertNotNull(s.getLinguagem());
+    	assertEquals("c", s.getLinguagem());
+    	assertEquals("c, for", s.getTags());
+    	assertEquals("for", s.getCodigo());
+    }
+    
+    @Test public void testSnippetWithoutCodigo(){
+    	String nome = "aa",linguagem = "c",codigo = "for",tags = "c, for";
+    	Snippet s = new Snippet();
+    	s.setLinguagem(linguagem);
+    	s.setNome(nome);
+    	s.setTags(tags);
+    	assertNull(s.getCodigo());
+    	assertNotNull(s.getNome());
+    	assertNotNull(s.getTags());
+    	assertNotNull(s.getLinguagem());
+    	assertEquals("c", s.getLinguagem());
+    	assertEquals("c, for", s.getTags());
+    	assertEquals("aa", s.getNome());
+    }
+    
     
     
     
